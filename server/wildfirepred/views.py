d7df18cb24526165a1b3a097d5d116556541b0ff
NVombat <nvombatkere@gmail.com>
2022-03-04 21:59:32 +0530

    client paths fixed, server urls established

--- server/wildfirepred/views.py
+++ server/wildfirepred/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def faq(request):
+    return render(request, "faq.html")
+
+def predict(request):
+    return render(request, "prediction.html")
+
+def pricing(request):
+    return render(request, "pricing.html")
