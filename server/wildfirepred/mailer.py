dbd44b68aa2e14a3162b0451aeabbd50cf9d5d3a
NVombat <nvombatkere@gmail.com>
2022-05-13 02:01:56 +0530

    email updates added

--- server/wildfirepred/mailer.py
+++ server/wildfirepred/mailer.py
@@ -6,9 +6,9 @@ load_dotenv()
 
 
 def send_feedback_mail(
+    result: str,
     email: str = "nv9824@srmist.edu.in",
-    name: str = "Test User",
-    message: str = "Test Message",
+    name: str = "Test User"
 ) -> None:
     """Sends Result Mail To User
 
@@ -23,13 +23,12 @@ def send_feedback_mail(
     backemail_add = os.getenv("BACKEND_MAIL_ADDR")
     backemail_pwd = os.getenv("BACKEND_MAIL_PWD")
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(backemail_add, backemail_pwd)
+    server = smtplib.SMTP_SSL("smtp.gmail.com",465)
+    server.login(backemail_add,backemail_pwd)
 
     # User mail subject, body and format of the mail - FROM ADMIN TO USER
     subject1 = "Predictive Modelling of Wildfires: Prediction Results"
-    body1 = f"Dear {name} \n\nThank you for using our services! \n\nResult: {message} \n\nHope you have a wonderful day! \n\nWarm Regards, \n\nThe Help Team \nWildfire Prediction Team"
+    body1 = f"Dear {name} \n\nThank you for using our services! \n\nResult: The probability of a wildfire occurring is {result} % \n\nHope you have a wonderful day! \n\nWarm Regards, \n\nThe Help Team \nWildfire Prediction Team"
     msg1 = f"Subject: {subject1}\n\n{body1}"
 
     server.sendmail(backemail_add, email, msg1)
